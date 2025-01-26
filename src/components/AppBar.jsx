6932415b15447a2abb07ff12dedfc60a2df3c5fb
watteja <mateyast@gmail.com>
2025-01-26 15:16:03 +0100

    Exercise 10.7: scrollable app bar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import Constants from "expo-constants";
 import AppBarTab from "./AppBarTab";
 import theme from "../theme";
@@ -16,8 +16,10 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab text="Repositories" />
-      <AppBarTab text="Sign in" />
+      <ScrollView horizontal>
+        <AppBarTab text="Repositories" />
+        <AppBarTab text="Sign in" />
+      </ScrollView>
     </View>
   );
 };
