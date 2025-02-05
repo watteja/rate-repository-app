cb0d0fa0390c41c6f76cff7455e2f24f802837dc
watteja <mateyast@gmail.com>
2025-02-05 18:50:24 +0100

    Exercise 10.15: storing the access token step2

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,4 +1,5 @@
 import { TextInput, Pressable, View, StyleSheet } from "react-native";
+import { useNavigate } from "react-router-native";
 import { useFormik } from "formik";
 import * as yup from "yup";
 
@@ -103,13 +104,15 @@ const SignInForm = ({ onSubmit }) => {
 
 const SignIn = () => {
   const [signIn] = useSignIn();
+  const navigate = useNavigate();
 
   const onSubmit = async (values) => {
     const { username, password } = values;
 
     try {
-      const { data } = await signIn({ username, password });
+      const data = await signIn({ username, password });
       console.log(data);
+      navigate("/");
     } catch (e) {
       console.log(e);
     }
--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,15 +1,25 @@
-import { useMutation } from "@apollo/client";
+import { useMutation, useApolloClient } from "@apollo/client";
+
 import { AUTHENTICATE } from "../graphql/mutations";
+import useAuthStorage from "./useAuthStorage";
 
 const useSignIn = () => {
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
   const [mutate, result] = useMutation(AUTHENTICATE);
 
   const signIn = async ({ username, password }) => {
-    return await mutate({
+    const { data } = await mutate({
       variables: {
         credentials: { username, password },
       },
     });
+
+    await authStorage.setAccessToken(data.authenticate.accessToken);
+
+    apolloClient.resetStore();
+
+    return data;
   };
 
   return [signIn, result];
