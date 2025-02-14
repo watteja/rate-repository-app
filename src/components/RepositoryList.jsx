af8d197836f16a8fb4a38ad6bad1bfab229702c8
watteja <mateyast@gmail.com>
2025-02-14 11:08:02 +0100

    Exercise 10.24: filtering the reviewed repositories list

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { FlatList, View, StyleSheet, Pressable } from "react-native";
 import { useNavigate } from "react-router-native";
 import { Picker } from "@react-native-picker/picker";
+import { Searchbar } from "react-native-paper";
+import { useDebounce } from "use-debounce";
 
 import RepositoryItem from "./RepositoryItem";
 import useRepositories from "../hooks/useRepositories";
@@ -40,57 +42,83 @@ const OrderPrinciple = {
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-const RepositoryListHeader = ({ onPick }) => {
+const RepositoryListHeader = ({ onChangeParams }) => {
   const [pickerValue, setPickerValue] = useState("latest");
+  const [searchKeyword, setSearchKeyword] = useState("");
+
   return (
-    <Picker
-      selectedValue={pickerValue}
-      onValueChange={(itemValue) => {
-        onPick(itemValue);
-        setPickerValue(itemValue);
-      }}
-    >
-      <Picker.Item label="Latest repositories" value="latest" />
-      <Picker.Item label="Highest rated repositories" value="highest" />
-      <Picker.Item label="Lowest rated repositories" value="lowest" />
-    </Picker>
+    <>
+      <Searchbar
+        value={searchKeyword}
+        placeholder="Search repositories"
+        onChangeText={(text) => {
+          setSearchKeyword(text);
+          onChangeParams(OrderPrinciple[pickerValue], text);
+        }}
+        mode="view"
+        style={{ margin: 10 }}
+      />
+      <Picker
+        selectedValue={pickerValue}
+        onValueChange={(itemValue) => {
+          setPickerValue(itemValue);
+          onChangeParams(OrderPrinciple[itemValue], searchKeyword);
+        }}
+      >
+        <Picker.Item label="Latest repositories" value="latest" />
+        <Picker.Item label="Highest rated repositories" value="highest" />
+        <Picker.Item label="Lowest rated repositories" value="lowest" />
+      </Picker>
+    </>
   );
 };
 
-export const RepositoryListContainer = ({ repositories, onPick }) => {
-  const navigate = useNavigate();
+export class RepositoryListContainer extends React.Component {
+  renderHeader = () => {
+    return <RepositoryListHeader onChangeParams={this.props.onChangeParams} />;
+  };
 
-  // Get the nodes from the edges array
-  const repositoryNodes = repositories
-    ? repositories.edges.map((edge) => edge.node)
-    : [];
+  render() {
+    // Get the nodes from the edges array (has to happen inside the render method to refresh)
+    const repositoryNodes = this.props.repositories
+      ? this.props.repositories.edges.map((edge) => edge.node)
+      : [];
 
-  return (
-    <FlatList
-      data={repositoryNodes}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem={({ item }) => (
-        <Pressable onPress={() => navigate(`/${item.id}`)}>
-          <RepositoryItem repository={item} />
-        </Pressable>
-      )}
-      ListHeaderComponent={<RepositoryListHeader onPick={onPick} />}
-      keyExtractor={(item) => item.id}
-    />
-  );
-};
+    return (
+      <FlatList
+        data={repositoryNodes}
+        ItemSeparatorComponent={ItemSeparator}
+        renderItem={({ item }) => (
+          <Pressable onPress={() => this.props.navigate(`/${item.id}`)}>
+            <RepositoryItem repository={item} />
+          </Pressable>
+        )}
+        ListHeaderComponent={this.renderHeader}
+        keyExtractor={(item) => item.id}
+      />
+    );
+  }
+}
 
 const RepositoryList = () => {
   const [orderPrinciple, setOrderPrinciple] = useState(OrderPrinciple.latest);
-  const { repositories } = useRepositories(orderPrinciple);
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [debouncedKeyword] = useDebounce(searchKeyword, 500);
+  const { repositories } = useRepositories(orderPrinciple, debouncedKeyword);
+  const navigate = useNavigate();
 
-  const handlePick = (value) => {
-    setOrderPrinciple(OrderPrinciple[value]);
+  const handleChangeParams = (newOrderPrinciple, newSearchKeyword) => {
+    setOrderPrinciple(newOrderPrinciple);
+    setSearchKeyword(newSearchKeyword);
   };
 
   // extracted "pure" component for easier testing, as advised by the materials
   return (
-    <RepositoryListContainer repositories={repositories} onPick={handlePick} />
+    <RepositoryListContainer
+      repositories={repositories}
+      onChangeParams={handleChangeParams}
+      navigate={navigate}
+    />
   );
 };
 
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -3,10 +3,10 @@ import { useQuery } from "@apollo/client";
 
 import { GET_REPOSITORIES } from "../graphql/queries";
 
-const useRepositories = ({ orderBy, orderDirection }) => {
+const useRepositories = ({ orderBy, orderDirection }, searchKeyword) => {
   const { data, error, loading } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: "cache-and-network",
-    variables: { orderBy, orderDirection },
+    variables: { orderBy, orderDirection, searchKeyword },
   });
   const [repositories, setRepositories] = useState();
 
